5e77db3858457c48c79237c31920b2f23669ad3d
Boris Chumichev <boris362@yandex.ru>
2015-05-31 15:17:56 +0300

    Sine test refactor

--- test/sine.test.js
+++ test/sine.test.js
@@ -4,22 +4,24 @@ var Sine = require('../build/signal-sources.js').Sine
   , GenericSignal = require('../build/signal-sources.js').GenericSignal
   , assert = require('assert')
 
-describe('Sine class instance', () => {
+describe('Sine class', () => {
   var sine = new Sine(1, 1)
   it('extends GenericSignal class', () => {
     assert(GenericSignal.prototype.isPrototypeOf(sine))
   })
-  it('has amplitude, frequency, phase, offset, startTime properties', () => {
-    assert.equal(sine.amplitude, 1)
-    assert.equal(sine.frequency, 1)
-    assert.equal(sine.phase, 0)
-    assert.equal(sine.offset, 0)
-    assert.equal(sine.startTime, 0)
-  })
-  describe('#resolve()', () => {
-    it('should resolve sine value at a given time', () => {
-      assert.equal(sine.resolve(0), 0)
-      assert.equal(sine.resolve(0.25), 1)
+  describe('Sine instance', () => {
+    it('has amplitude, frequency, phase, offset, startTime properties', () => {
+      assert.equal(sine.amplitude, 1)
+      assert.equal(sine.frequency, 1)
+      assert.equal(sine.phase, 0)
+      assert.equal(sine.offset, 0)
+      assert.equal(sine.startTime, 0)
+    })
+    describe('#resolve()', () => {
+      it('should resolve sine value at a given time', () => {
+        assert.equal(sine.resolve(0), 0)
+        assert.equal(sine.resolve(0.25), 1)
+      })
     })
   })
 })
